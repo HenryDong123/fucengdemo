2a1497e78476814ff4d7a2605ca85291248a6542
HenryDong123 <943082205@qq.com>
2018-10-24 00:08:49 +0800

    添加再次点击按钮关闭浮层2.0

--- main.js
+++ main.js
@@ -2,16 +2,17 @@ $(clickMe).on('click', function () {
     if ($(popover).is(':hidden')) {
         $(popover).show()
 
-        $(document).one('click', function () {
-            $(popover).hide()
 
-        })
 
 
     } else {
         $(popover).hide()
 
     }
+    $(document).one('click', function () {
+        $(popover).hide()
+
+    })
 
 
 })
